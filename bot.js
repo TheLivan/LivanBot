8ca1c09f813afb46e4b216b6e8575360da03d5d1
TheLivan <rtg.kirepifanov.corp@gmail.com>
2019-06-19 04:17:35 +0300

    gg

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const fs = require('fs')
 const bot = new Discord.Client()
 bot.commands = new Discord.Collection() 
-const xpclan = require("/utils/errors.js");
+const xpclan = require("/utils/xpclan.js");
 
 fs.readdir('./commands', (err, files) => { // чтение файлов в папке commands
     if (err) console.log(err)   
