c059c4f6518722c1c0e18072cd8fc3322f08649e
TheLivan <rtg.kirepifanov.corp@gmail.com>
2019-06-20 22:36:55 +0300

    рр

--- commands/privatemessage.js
+++ commands/privatemessage.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js') // подключение discord.js к ф
 
 module.exports.run = async (bot, message, args) => {
 
-    message.guild.members.get(args[0]).send(args[1]);
+    message.guild.members.find(x => x.name === args[0]).send(args[1]);
 }
 
 module.exports.help = {
