10b1e3da9d9789db9c2252d53019aa0ec310a7b8
Alexander Pistoletov <50182799+TheLivan@users.noreply.github.com>
2019-07-02 00:43:54 +0300

    aue

--- bot.js
+++ bot.js
@@ -67,17 +67,9 @@ Campy the Livan Bot
 '----`)
     console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
     //bot.user.setActivity("my code", { type: "STREAMING", url: "https://vk.com/exbo_notifier" })
-    //bot.user.setPresence({ status: 'dnd', game: { name: 'подписывайтесь на уведомитель', type: 0 } })
-
-    bot.user.setPresence({
-        game: { 
-            name: 'my code',
-            type: 'STREAMING',  
             url: 'https://vk.com/thelivan'
-        },
-        status: 'idle',
-    })
-
+    bot.user.setPresence({ status: 'dnd', game: { name: 'подписывайтесь на уведомитель', type: 0 } })
+    
     var interval = setInterval(function () {
         bot.channels.get('587243104625491970').send('ban huan').delete(10000)
     }, 1 * 900000);
