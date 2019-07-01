fb63c0ef28818aa4710f986a0951f2c46e6ae0f1
Alexander Pistoletov <50182799+TheLivan@users.noreply.github.com>
2019-07-02 00:26:40 +0300

    my life

--- commands/privatemessage.js
+++ commands/privatemessage.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js') // подключение discord.js к ф
 
 module.exports.run = async (bot, message, args) => {
     let messageArgs = args.slice(0)
-    bot.guilds.find(x => x.id === process.env.MAIN_GUILD).members.find(x => x.id === args[0]).send(messageArgs.join);
+    bot.guilds.find(x => x.id === process.env.MAIN_GUILD).members.find(x => x.id === args[0]).send(messageArgs.join(' '));
 }
 
 module.exports.help = {
