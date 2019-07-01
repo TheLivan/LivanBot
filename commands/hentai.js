466dbd7389f770558953486895748b56ace1dee3
Alexander Pistoletov <50182799+TheLivan@users.noreply.github.com>
2019-07-01 23:55:50 +0300

    hentai

---
+++ commands/hentai.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const { get } = require("snekfetch"); 
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => {
+    try {
+        get('https://nekos.life/api/v2/img/hentai').then(res => {
+            const embed = new Discord.RichEmbed()
+            .setImage(res.body.url)
+            return message.channel.send({embed});
+        });
+    } catch(err) {
+        return message.channel.send('Ниработает');
+    }
+}
+  
+module.exports.help = {
+  name: "hentai"
+}   
