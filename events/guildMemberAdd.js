690fd76ab8d761f49062700c2bedbc3dca47d8f2
Alexander Pistoletov <50182799+TheLivan@users.noreply.github.com>
2019-07-02 12:53:56 +0300

    aa

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,8 +4,7 @@ module.exports = (guild, member) => {
     console.log(member.user.username + " joined " + guild.name);
     const embed = new Discord.RichEmbed()
     .setTitle('Новый человек!')
-    .setDescription('@' + member.user.username + ' присоединился к нам!')
+    .setDescription('<@" + member.id + "> присоединился к нам!')
     .addField('Если нужен экскурс по серверу, то обрати внимание на #console ', 'Приятного общения!', true)
-    bot.channels.get('587243104625491970').send('@' + member.user.username)
     bot.channels.get('587243104625491970').send(embed);
 }
