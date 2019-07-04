bf001f2c59bc6890308ef702c80383d7782d627e
Alexander Pistoletov <50182799+TheLivan@users.noreply.github.com>
2019-07-04 03:06:14 +0300

    ss

--- commands/slots.js
+++ commands/slots.js
@@ -5,7 +5,7 @@ const bot = new Discord.Client()
 const slots = ['🍇', '🍊', '🍐', '🍒', '🍋'];   
 
 module.exports.run = async (bot, message, args) => {
-	require("../utils/tempchannel.js").noPerms(message, args, message.member.nickname)
+	require("../utils/tempchannel.js").noPerms(message, args, '11')
 	
     var slot1 = slots[Math.floor(Math.random() * slots.length)];
 	var slot2 = slots[Math.floor(Math.random() * slots.length)];
