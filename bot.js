484b8ffdcc9e263d5cb72c499c1a2bfb5e88f3ec
Alexander Pistoletov <50182799+TheLivan@users.noreply.github.com>
2019-07-06 20:02:23 +0300

    dd

--- bot.js
+++ bot.js
@@ -50,6 +50,20 @@ bot.on('message', async message => {
     let command = messageArray[0] // команда после префикса
     let args = messageArray.slice(1) // аргументы после команды
 
+    if (message.content === '/join') {
+        // Only try to join the sender's voice channel if they are in one themselves
+        if (message.member.voiceChannel) {
+          message.member.voiceChannel.join()
+            .then(connection => { // Connection is an instance of VoiceConnection
+              message.reply('I have successfully connected to the channel!');
+            })
+            .catch(console.log);
+        } else {
+          message.reply('You need to join a voice channel first!');
+        }
+      }
+
+
     let command_file = bot.commands.get(command.slice(prefix.length)) // получение команды из коллекции
     if (command_file) command_file.run(bot, message, args)
 })
